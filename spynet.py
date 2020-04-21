37fae708b17bbdd02620f460ffcc16f13a3b8f2f
Paul Galatic <pgalatic@gmail.com>
2020-04-21 09:18:00 -0400

    Initial forked commit

--- spynet.py
+++ spynet.py
@@ -16,7 +16,7 @@ assert(int(str('').join(torch.__version__.split('.')[0:2])) >= 13) # requires at
 
 torch.set_grad_enabled(False) # make sure to not compute gradients for computational performance
 
-torch.backends.cudnn.enabled = True # make sure to use cudnn for computational performance
+torch.backends.cudnn.enabled = False # make sure to use cudnn if possible for computational performance
 
 ##########################################################
 
@@ -142,9 +142,6 @@ def estimate(tenFirst, tenSecond):
 
 	intWidth = tenFirst.shape[2]
 	intHeight = tenFirst.shape[1]
-
-	assert(intWidth == 1024) # remember that there is no guarantee for correctness, comment this line out if you acknowledge this and want to continue
-	assert(intHeight == 416) # remember that there is no guarantee for correctness, comment this line out if you acknowledge this and want to continue
 
 	tenPreprocessedFirst = tenFirst.cuda().view(1, 3, intHeight, intWidth)
 	tenPreprocessedSecond = tenSecond.cuda().view(1, 3, intHeight, intWidth)
